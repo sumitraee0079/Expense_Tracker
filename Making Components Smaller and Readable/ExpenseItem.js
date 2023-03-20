01f4dc715c0a635be9efee9a69df81c558c24c1c
sumitraee0079 <31108006+sumitraee0079@users.noreply.github.com>
2023-03-21 02:58:01 +0530

    Update ExpenseItem.js

--- Making Components Smaller and Readable/ExpenseItem.js
+++ Making Components Smaller and Readable/ExpenseItem.js
@@ -1,19 +1,19 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import ExpenseDetails from './ExpenseDetails'
 
 const ExpenseItem = (props) => {
+    return (
 
-    
+<div className='expense-item'>
 
-    return <div className='expense-item'>
-    <ExpenseDate date={props.date}></ExpenseDate>
-    <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-    </div>
-    <div className='expense-item__price'>Rs{props.amount}</div>
-    <div>{props.LocationOfExpenditure}</div>
+<ExpenseDate date ={props.date} />
 
-    </div>
+<ExpenseDetails amount={props.amount}  LocationOfExpenditure={props.LocationOfExpenditure}   title={props.title} />
+
+</div>
+
+)
     
   }
   
