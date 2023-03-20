b33e765cc7a8a34ef4a6415fea793954bf024299
sumitraee0079 <31108006+sumitraee0079@users.noreply.github.com>
2023-03-21 01:06:28 +0530

    Update ExpenseItem.js

--- Passing Data/ExpenseItem.js
+++ Passing Data/ExpenseItem.js
@@ -3,11 +3,11 @@ import './ExpenseItem.css'
 const ExpenseItem = (props) => {
 
     return <div className='expense-item'>
-    <div>{props.expenseDate.toISOString()}</div>
+    <div>{props.date.toISOString()}</div>
     <div className='expense-item__description'>
-        <h2>{props.expenseTitle}</h2>
+        <h2>{props.title}</h2>
     </div>
-    <div className='expense-item__price'>Rs{props.expenseAmount}</div>
+    <div className='expense-item__price'>Rs{props.amount}</div>
     <div>{props.LocationOfExpenditure}</div>
 
     </div>
