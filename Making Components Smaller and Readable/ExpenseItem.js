8177402fbd11e45300ca69dac413b0654af1499f
sumitraee0079 <31108006+sumitraee0079@users.noreply.github.com>
2023-03-21 02:41:19 +0530

    Update ExpenseItem.js

--- Making Components Smaller and Readable/ExpenseItem.js
+++ Making Components Smaller and Readable/ExpenseItem.js
@@ -1,17 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = (props) => {
 
-    const month = props.date.toLocalString('en-US', {month: 'long'})
-    const day = props.date.toLocalString('en-US', {day: '2-digit'})
-    const year = props.date.getFullYear()
+    
 
     return <div className='expense-item'>
-    <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-    </div>
+    <ExpenseDate date={props.date}></ExpenseDate>
     <div className='expense-item__description'>
         <h2>{props.title}</h2>
     </div>
