a698abcf55cf4b18b30421ce7414ee549b330559
sumitraee0079 <31108006+sumitraee0079@users.noreply.github.com>
2023-03-21 02:34:43 +0530

    Create ExpenseItem.js

---
+++ Making Components Smaller and Readable/ExpenseItem.js
@@ -0,0 +1,25 @@
+import './ExpenseItem.css'
+
+const ExpenseItem = (props) => {
+
+    const month = props.date.toLocalString('en-US', {month: 'long'})
+    const day = props.date.toLocalString('en-US', {day: '2-digit'})
+    const year = props.date.getFullYear()
+
+    return <div className='expense-item'>
+    <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+    </div>
+    <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+    </div>
+    <div className='expense-item__price'>Rs{props.amount}</div>
+    <div>{props.LocationOfExpenditure}</div>
+
+    </div>
+    
+  }
+  
+  export default ExpenseItem
