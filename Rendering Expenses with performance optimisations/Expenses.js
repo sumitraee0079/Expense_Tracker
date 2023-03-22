ac763f57c9dd72a9e8cb9c19b09f90af89711282
sumitraee0079 <31108006+sumitraee0079@users.noreply.github.com>
2023-03-23 05:04:08 +0530

    Create Expenses.js

---
+++ Rendering Expenses with performance optimisations/Expenses.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react' 
+import './Expenses.css'
+import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
+import ExpenseList from './ExpenseList'
+import ExpensesChart from './ExpensesChart'
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020')
+    const filterChangeHandler = (selectedYear) => {
+      setFilteredYear(selectedYear)
+    }
+
+    const filteredExpenses = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear
+    })
+
+    
+    
+     return <div>
+        
+    <li>
+    <Card className="expenses">
+    <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}></ExpensesFilter>
+
+    {props.items.map((expense) => {
+  <ExpenseItem title={expense.title}
+  amount={expense.amount}
+  date={expense.date} />
+}
+  
+  )}
+
+    </Card></li>
+    </div>
+    
+}
+
+export default Expenses
